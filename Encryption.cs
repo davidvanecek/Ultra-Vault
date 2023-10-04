af21e36d549d0763a4c90eee78c34f39a51b3d75
FoodWithoutTaste <88619470+FoodWithoutTaste@users.noreply.github.com>
2023-10-04 09:55:54 +0200

    Encryption

---
+++ Encryption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crypto.AES;
+namespace Hesla
+{
+    class Encryption
+    {
+        public string EncryptString(string password)
+        {
+            using (AES aes = new AES("SHortKEy"))
+            {
+                return aes.Encrypt(password);
+            }
+        }
+        public string DecryptString(string password)
+        {
+            using (AES aes = new AES("SHortKEy"))
+            {
+                return aes.Decrypt(password);
+            }
+        }
+    }
+}
