ae2d76e3c076554b9ebf857f27f2d51ca14166c4
Adam Doležal <dolezal.adam@merkatos.cz>
2023-10-04 11:29:30 +0200

    change loader

--- UltraVault/Loader.Designer.cs
+++ UltraVault/Loader.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loader));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroSetProgressBar1 = new MetroSet_UI.Controls.MetroSetProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,6 +45,30 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // metroSetProgressBar1
+            // 
+            this.metroSetProgressBar1.BackgroundColor = System.Drawing.Color.White;
+            this.metroSetProgressBar1.BorderColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.DisabledBackColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.DisabledBorderColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.DisabledProgressColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.ForeColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.IsDerivedStyle = true;
+            this.metroSetProgressBar1.Location = new System.Drawing.Point(197, 398);
+            this.metroSetProgressBar1.Maximum = 100;
+            this.metroSetProgressBar1.Minimum = 0;
+            this.metroSetProgressBar1.Name = "metroSetProgressBar1";
+            this.metroSetProgressBar1.Orientation = MetroSet_UI.Enums.ProgressOrientation.Horizontal;
+            this.metroSetProgressBar1.ProgressColor = System.Drawing.Color.Red;
+            this.metroSetProgressBar1.Size = new System.Drawing.Size(404, 23);
+            this.metroSetProgressBar1.Style = MetroSet_UI.Enums.Style.Light;
+            this.metroSetProgressBar1.StyleManager = null;
+            this.metroSetProgressBar1.TabIndex = 2;
+            this.metroSetProgressBar1.Text = "metroSetProgressBar1";
+            this.metroSetProgressBar1.ThemeAuthor = "Narwin";
+            this.metroSetProgressBar1.ThemeName = "MetroLite";
+            this.metroSetProgressBar1.Value = 0;
+            // 
             // Loader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
@@ -51,6 +76,7 @@
             this.BackgroundColor = System.Drawing.Color.Black;
             this.BorderColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroSetProgressBar1);
             this.Controls.Add(this.pictureBox1);
             this.HeaderColor = System.Drawing.Color.Red;
             this.Name = "Loader";
@@ -67,6 +93,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroSet_UI.Controls.MetroSetProgressBar metroSetProgressBar1;
     }
 }
 
--- UltraVault/Loader.cs
+++ UltraVault/Loader.cs
@@ -13,9 +13,41 @@ namespace UltraVault
 {
     public partial class Loader : MetroSetForm
     {
+        private Timer progressBarTimer;
+        private int progressBarValue;
+
         public Loader()
         {
             InitializeComponent();
+
+            // Inicializace Timeru
+            progressBarTimer = new Timer();
+            progressBarTimer.Interval = 30; // Interval v milisekundách
+            progressBarTimer.Tick += new EventHandler(ProgressBarTimer_Tick);
+            
+
+
+        }
+
+        private void ProgressBarTimer_Tick(object sender, EventArgs e)
+        {
+            // Inkrementujeme hodnotu progressBaru
+            progressBarValue++;
+            metroSetProgressBar1.Value = progressBarValue;
+            metroSetProgressBar1.ProgressColor = Color.Red;
+
+            // Pokud jsme dosáhli 100, zastavíme Timer
+            if (progressBarValue >= 100)
+            {
+                progressBarTimer.Stop();
+
+                // Otevření Form1
+                UltraVault mainForm = new UltraVault();
+                mainForm.Show();
+
+                // Zavření okna
+                this.Hide();
+            }
         }
 
         private async void Loader_Load(object sender, EventArgs e)
@@ -23,12 +55,8 @@ namespace UltraVault
             // Čekání na 3 sekundy
             await Task.Delay(3000);
 
-            // Otevření Form1
-            UltraVault mainForm = new UltraVault();
-            mainForm.Show();
-
-            // Zavření okna
-            this.Hide();
+            // Spustíme Timer
+            progressBarTimer.Start();
         }
     }
 }
